a3fe3f63c5a1b1fd286ebe8cb6b0326664f3d291
YogeshDekate25 <sonudekate748@gmail.com>
2023-01-24 12:48:21 +0530

    Done with Day 1

--- Day1ofPractice.c
+++ Day1ofPractice.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+
 //Calculating perimeter of a rectangle with input function
-int main(void)
+/*int main(void)
 {
     int a;
     int b;
@@ -13,5 +14,22 @@ int main(void)
 
     printf("The perimeter of rectangle is %d \n", a+b);
 
+    return 0;
+}*/
+
+//Checking Whethere the given number is odd or even
+int main(void)
+{
+    int x;
+    printf("Enter the number");
+    scanf("%d",&x);
+    
+    if (x%2==0)
+    {
+        printf("The number is even");
+    }
+    else{
+        printf("The number is odd ");
+    }
     return 0;
 }
