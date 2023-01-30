9c9823fa840570b6e138d80ba3b86d219bcb55cd
YogeshDekate25 <sonudekate748@gmail.com>
2023-01-30 11:30:27 +0530

    Calculating areas

--- Day3ofPractice.c
+++ Day3ofPractice.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-// if user is Indian Print "Namaste" and if user is French print "Bonjour".
+/* if user is Indian Print "Namaste" and if user is French print "Bonjour".
 void Indian(){
     printf("Namaste\n");
 }
@@ -19,4 +19,28 @@ int main(void){
     else{
         French();
     }
+}*/
+
+// Calculate Area of circle, Square and Rectangle
+
+int Square(int a){
+
+   printf("Area of Square is %d\n", a*a);
+}
+
+int Rectangle(int l , int b){
+    printf("Area of Rectangle is %d\n" , l*b);
+}
+
+float Circle(float r){
+    float pie = 3.14f;
+    printf("The are of circle is %f\n", pie*r*r);
+}
+
+int main (void){
+    int a,l,b,r;
+    
+    printf("Enter the value of side : ");
+    scanf("%d",&a);
+    Square(a);
 }
